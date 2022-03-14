d057b07c30f855cc6327da9dcdab6cbca6887c74
reigny98 <reigny2016@gmail.com>
2022-03-13 21:42:20 -1200

    added program

---
+++ Uc7EmpComputeWages/UC-7-emp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace UseCase_7_Employee_Wage
+{
+    public class EmployeeWage
+    {
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_Of_Working_Days = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
+
+        public static int computeEmpWage()
+        {
+            //variable 
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_Of_Working_Days)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("DAy#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage();
+        }
+    }
+}
